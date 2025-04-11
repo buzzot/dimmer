01ff68055d104ea6ee7a2e1e601e0478d9250a74
mrakhmatullin <marsello83@hotmail.com>
2025-04-11 08:08:18 -0700

    For demo

--- dimmer_test/dimmer_test/settings.py
+++ dimmer_test/dimmer_test/settings.py
@@ -9,8 +9,10 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
-
+import os
 from pathlib import Path
+
+from dimmer_test import dimmer_test
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +27,17 @@ SECRET_KEY = 'django-insecure-qx4s$6lv*f3yl=_u2vnikx&ud^vv+dh7!^bq)0p+%5-g9t^+@6
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
+
+import dj_database_url
+
+DATABASES = {
+    'default': dj_database_url.config(conn_max_age=600, ssl_require=True)
+}
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+DJANGO_SETTINGS_MODULE=dimmer_test.settings
 
 
 # Application definition
